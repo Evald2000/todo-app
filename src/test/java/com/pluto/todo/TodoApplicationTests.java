adc4b0d1c275a0f0c8d09edfa7735b54a543c519
Evald2000 <evalddenhof@gmail.com>
2023-10-23 11:58:55 +0200

    first push

---
+++ src/test/java/com/pluto/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluto.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
